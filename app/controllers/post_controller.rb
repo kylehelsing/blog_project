3c4a6b075e14c159d5be85955519a643f4e80b40
Kyle Helsing <kylehelsing@gmail.com>
2017-03-21 14:22:58 -0400

    init

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,22 @@
+class PostController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
